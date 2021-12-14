38a4a83628fcd0bbedc2e93be7a663f233a40e64
parkeeeeeeeeer <46697381+parkeeeeeeeeer@users.noreply.github.com>
2021-12-14 16:45:54 -0500

    comments for main

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# importing the module with all functions
 import mod
 
 '''main program import the above module to read the attached file
@@ -5,8 +6,12 @@ import mod
   and find the 10 most used words in the file. Create a file called 
   ‘mostusedwords.txt’ and write these 10 most used words to this file.'''
 
-x = mod.text_string('valleyoffear.txt')
-most = mod.most_used(x)
-print(most)
+# creates a variable that makes the valleyoffear text a string and stores as x
 
+x = mod.text_string('valleyoffear.txt')
+
+# most stores the top ten words in a list without the amount of words just the words themselves
+most = mod.most_used(x)
+
+# this writes that the most list into the mostusedwords.txt
 mod.new_line('mostusedwords.txt', most)
